d302b0fadfddc47d46845233f0759b88d19df6a9
Eduardo Knop <eduk.coelho@hotmail.com>
2025-08-04 07:56:14 -0300

    Initial Commit

---
+++ iTravelTests/iTravelTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTravelTests.swift
+//  iTravelTests
+//
+//  Created by Eduardo Knop on 04/08/25.
+//
+
+import Testing
+@testable import iTravel
+
+struct iTravelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
