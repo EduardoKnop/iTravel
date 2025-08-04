b330d9e55904f3b44ca0feefa7bd8ecc9bec89cf
Eduardo Knop <eduk.coelho@hotmail.com>
2025-08-04 20:32:55 -0300

    Creating Base View

---
+++ iTravel/HomeTableViewHeader.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTableViewHeader.swift
+//  iTravel
+//
+//  Created by Eduardo Knop on 04/08/25.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
+}
--- iTravel/ViewController.swift
+++ iTravel/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var travelTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        travelTableView.dataSource = self
+        travelTableView.delegate = self
+        
+        view.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self, options: nil)?.first as? HomeTableViewHeader
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Travel \(indexPath.row)"
+        
+        return cell
+    }
+}
